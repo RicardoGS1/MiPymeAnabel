082e530e2cb4b29d732927bc376e6a4c8a4ffe6e
RicardoGS1 <granadosacerioricardo@gmail.com>
2024-11-26 18:34:53 -0500

    add SearchBar and filter completed

--- composeApp/src/commonMain/kotlin/com/virtualworld/mipymeanabel/ui/screen/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/virtualworld/mipymeanabel/ui/screen/home/HomeScreen.kt
@@ -1,32 +1,57 @@
 package com.virtualworld.mipymeanabel.ui.screen.home
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,6 +59,7 @@ import coil3.compose.AsyncImage
 import com.virtualworld.mipymeanabel.data.model.Product
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.annotation.KoinExperimentalAPI
+import kotlin.text.toFloat
 
 @OptIn(KoinExperimentalAPI::class)
 @Composable
@@ -43,21 +69,82 @@ fun HomeScreen(homeViewModel: HomeViewModel = koinViewModel()) {
 
 
     Surface(modifier = Modifier.fillMaxSize()) {
+
         val listState = rememberLazyGridState()
-        LazyVerticalGrid(
-            state = listState,
-            columns = GridCells.Adaptive(120.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            modifier = Modifier.padding(horizontal = 8.dp)
-        ) {
-            items(products, key = { it.idp }) {
-                ProductItem(
-                    product = it,
-                    onProductClicked = {},
-                )
+
+
+        val searchBarVisible =
+            remember { derivedStateOf { (listState.firstVisibleItemScrollOffset < 2) } }
+
+
+
+        Column() {
+
+            // Barra de búsqueda
+            AnimatedVisibility(visible = (searchBarVisible.value)) {
+                SearchBar()
             }
 
+
+            // Contenido desplazable
+            LazyVerticalGrid(
+                state = listState,
+                columns = GridCells.Adaptive(120.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+
+            ) {
+
+                items(products, key = { it.idp }) {
+                    ProductItem(
+                        product = it,
+                        onProductClicked = {},
+                    )
+                }
+
+            }
+
+        }
+    }
+
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBar() {
+    var searchText by remember { mutableStateOf("") }
+
+
+
+    Row(
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp).padding(bottom = 4.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        OutlinedTextField(
+            value = searchText,
+            onValueChange = { searchText = it },
+            label = { Text("Buscar") },
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.Search,
+                    contentDescription = "Buscar..."
+                )
+            },
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 16.dp),
+            shape = RoundedCornerShape(16.dp),
+        )
+
+        IconButton(onClick = { /* Acción al hacer clic en el botón */ }) {
+            Icon(
+                imageVector = Icons.Filled.Menu, // Icono del botón
+                contentDescription = "Filtrar"
+            )
         }
 
 
@@ -100,7 +187,7 @@ fun ProductItem(product: Product, onProductClicked: () -> Unit) {
                 text = product.priceUsd + " USD",
                 maxLines = 1,
                 textAlign = TextAlign.End,
-                modifier = Modifier.padding(horizontal =  8.dp).fillMaxWidth()
+                modifier = Modifier.padding(horizontal = 8.dp).fillMaxWidth()
             )
 
         }
--- composeApp/src/commonMain/kotlin/com/virtualworld/mipymeanabel/ui/screen/main/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/com/virtualworld/mipymeanabel/ui/screen/main/MainScreen.kt
@@ -1,11 +1,25 @@
 package com.virtualworld.mipymeanabel.ui.screen.main
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.navigation.compose.rememberNavController
@@ -27,9 +41,12 @@ fun MainScreen() {
             modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
         ) { paddingValues ->
 
+
+
             AppNavHost(navController, paddingValues)
 
 
         }
     }
 }
+
