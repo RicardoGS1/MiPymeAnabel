b4b617c8ca55468b00e6806883bb637a1f6b3983
RicardoGS1 <granadosacerioricardo@gmail.com>
2024-12-09 17:13:44 -0500

    -view detail screen created

--- composeApp/src/commonMain/kotlin/com/virtualworld/mipymeanabel/ui/screen/detail/DetailScreen.kt
+++ composeApp/src/commonMain/kotlin/com/virtualworld/mipymeanabel/ui/screen/detail/DetailScreen.kt
@@ -1,16 +1,58 @@
 package com.virtualworld.mipymeanabel.ui.screen.detail
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import com.virtualworld.mipymeanabel.data.dto.ProductAll
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 
 @Composable
 fun DetailScreen( detailViewModel : DetailViewModel ){
 
-    detailViewModel.a()
+    val productState by detailViewModel.productState.collectAsState()
 
-    Text(text = "dettalle")
+    Column(modifier = Modifier
+        .fillMaxSize()) {
+        AsyncImage(
+            model = productState.image,
+            contentDescription = productState.name,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxWidth().aspectRatio(2.5f / 2f)
+                .clip(RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp))
 
 
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Column(Modifier.padding(16.dp)) {
+            Text(
+                text = productState.name,
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = productState.detail)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "Price: $${productState.priceUsd}")
 
+        }
+
+    }
 }
+
+
--- composeApp/src/commonMain/kotlin/com/virtualworld/mipymeanabel/ui/screen/detail/DetailViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/virtualworld/mipymeanabel/ui/screen/detail/DetailViewModel.kt
@@ -1,8 +1,27 @@
 package com.virtualworld.mipymeanabel.ui.screen.detail
 
 import androidx.lifecycle.ViewModel
+import com.virtualworld.mipymeanabel.data.dto.ProductAll
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class DetailViewModel( private val productId : String ) : ViewModel() {
+
+    val productState : StateFlow<ProductAll> = MutableStateFlow<ProductAll>(ProductAll(
+        idp= 10.toString(),
+        name="Rollo de mantel AA-3226A",
+        priceMn= 10.toString(),
+        priceUsd= 92.toString(),
+        detail="Medida: 1.37 m x 20 m",
+        available= 1.toString(),
+        image="https://drive.usercontent.google.com/download?id=1fcG6jJ8egGqHgUklr-tBNLqTDXB8JaY5&export=download&confirm=t&uuid=4edb6c5d-3af2-4d99-b6e2-973a307f11ae",
+        category="Cocina",
+        favorite = true,
+        cart = true
+    )).asStateFlow()
 
     init {
         println(productId)
