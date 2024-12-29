e71e4ef2c4669c7951fa40747415afeda972a954
RicardoGS1 <granadosacerioricardo@gmail.com>
2024-12-29 13:46:58 -0500

    - modifier padding for full screen in auth

--- composeApp/src/commonMain/kotlin/com/virtualworld/mipymeanabel/ui/screen/main/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/com/virtualworld/mipymeanabel/ui/screen/main/MainScreen.kt
@@ -1,6 +1,14 @@
 package com.virtualworld.mipymeanabel.ui.screen.main
 
+import androidx.compose.animation.core.copy
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.waterfall
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -8,6 +16,8 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.virtualworld.mipymeanabel.navegation.AppNavHost
 
@@ -17,19 +27,7 @@ import com.virtualworld.mipymeanabel.navegation.AppNavHost
 fun MainScreen() {
 
     val navController = rememberNavController()
-
-
-
-
-//
-//    BackHandler {
-//        // Navega a la pantalla de inicio
-//        navController.popBackStack(
-//            navController.graph.startDestinationRoute ,
-//            false // inclusive: false para no incluir la pantalla de inicio en la eliminación
-//        )
-//    }
-
+    val paddingStatusBar = WindowInsets.statusBars.asPaddingValues()
 
     Surface(modifier = Modifier.fillMaxSize()) {
         val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
@@ -37,12 +35,20 @@ fun MainScreen() {
         Scaffold(
             bottomBar = { BottomBarView(navController) },
             topBar = { TopBarView(scrollBehavior, navController) },
-            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
+            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+            contentWindowInsets = WindowInsets.waterfall
         ) { paddingValues ->
 
+            val bottomPadding = paddingValues.calculateBottomPadding() - 16.dp // Reduce 16.dp al margen inferior
 
+            val modifiedPaddingValues = PaddingValues(
+                start = paddingValues.calculateStartPadding(LayoutDirection.Ltr),
+                top = paddingValues.calculateTopPadding(),
+                end = paddingValues.calculateEndPadding(LayoutDirection.Ltr),
+                bottom = bottomPadding
+            )
 
-            AppNavHost(navController, paddingValues)
+            AppNavHost(navController, modifiedPaddingValues)
 
 
         }
