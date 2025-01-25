5c20fc5e73688616701df1b42945ef72c8965b42
RicardoGS1 <granadosacerioricardo@gmail.com>
2025-01-25 10:20:05 -0500

    - created file

---
+++ composeApp/src/commonMain/kotlin/com/virtualworld/mipymeanabel/ui/screen/detailOrder/DetailOrderScreen.kt
@@ -0,0 +1,8 @@
+package com.virtualworld.mipymeanabel.ui.screen.detailOrder
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DetailOrderScreen( detailOrderViewModel: DetailOrderViewModel ){
+
+}
---
+++ composeApp/src/commonMain/kotlin/com/virtualworld/mipymeanabel/ui/screen/detailOrder/DetailOrderViewModel.kt
@@ -0,0 +1,14 @@
+package com.virtualworld.mipymeanabel.ui.screen.detailOrder
+
+import androidx.lifecycle.ViewModel
+import com.virtualworld.mipymeanabel.domain.useCase.DeletedOrderUseCase
+
+class DetailOrderViewModel(private val deletedOrderUseCase: DeletedOrderUseCase) : ViewModel() {
+
+
+
+
+
+
+
+}
