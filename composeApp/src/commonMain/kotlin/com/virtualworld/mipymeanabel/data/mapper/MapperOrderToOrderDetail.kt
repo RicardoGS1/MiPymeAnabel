4ae856df546d8cfd0d48ce78b0c69ef39f994cfa
RicardoGS1 <granadosacerioricardo@gmail.com>
2025-01-25 19:16:08 -0500

    -  refactor

---
+++ composeApp/src/commonMain/kotlin/com/virtualworld/mipymeanabel/data/mapper/MapperOrderToOrderDetail.kt
@@ -0,0 +1,47 @@
+package com.virtualworld.mipymeanabel.data.mapper
+
+import com.virtualworld.mipymeanabel.data.dto.Order
+import com.virtualworld.mipymeanabel.data.dto.OrderProducts
+import com.virtualworld.mipymeanabel.domain.models.OrderDetail
+
+fun Order.toOrderDetail() : OrderDetail {
+
+    return OrderDetail(
+
+        number = this.number,
+        state = this.state,
+        dateOrder = this.dateOrder,
+        dateDelivery = this.dateDelivery,
+        importTotal = calculateTotalImport(this.listOrderProducts).toString(),
+        unitTotal =  calculateTotalUnit(this.listOrderProducts) .toString(),
+        listOrderProducts = this.listOrderProducts
+
+    )
+
+
+
+
+
+
+}
+
+fun calculateTotalUnit(listOrderProducts: List<OrderProducts>): Float {
+
+    var totalUsd = 0.0f
+
+    listOrderProducts.forEach { product ->
+        totalUsd += product.unit.toInt()
+    }
+    return totalUsd
+}
+
+fun calculateTotalImport(listOrderProducts: List<OrderProducts>): Float {
+
+    var totalUsd = 0.0f
+
+    listOrderProducts.forEach { product ->
+        val priceUsd = product.priceUsd.toFloatOrNull() ?: 0.0f
+        totalUsd += priceUsd * product.unit.toInt()
+    }
+    return totalUsd
+}
--- composeApp/src/commonMain/kotlin/com/virtualworld/mipymeanabel/data/repository/OrderRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/com/virtualworld/mipymeanabel/data/repository/OrderRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.virtualworld.mipymeanabel.data.repository
 
 import com.virtualworld.mipymeanabel.data.dto.Order
 import com.virtualworld.mipymeanabel.data.dto.OrderProducts
+import com.virtualworld.mipymeanabel.data.mapper.toOrderDetail
 import com.virtualworld.mipymeanabel.data.model.NetworkResponseState
 import com.virtualworld.mipymeanabel.data.source.remote.FirebaseDataSource
 import com.virtualworld.mipymeanabel.data.utils.generateRandomCode
@@ -35,28 +36,19 @@ class OrderRepositoryImpl(
     ): Flow<NetworkResponseState<OrderDetail>> {
 
 
-
         return firebaseDataSource.getOrderById(uid, orderId).map {
 
-            when(it){
-                is NetworkResponseState.Error -> {NetworkResponseState.Error(it.exception)}
-                is NetworkResponseState.Loading -> {NetworkResponseState.Loading}
+            when (it) {
+                is NetworkResponseState.Error -> {
+                    NetworkResponseState.Error(it.exception)
+                }
+
+                is NetworkResponseState.Loading -> {
+                    NetworkResponseState.Loading
+                }
+
                 is NetworkResponseState.Success -> {
-
-                   val mapOrderDetail = OrderDetail(
-
-                       number = it.result.number,
-                       state = it.result.state,
-                       dateOrder = it.result.dateOrder,
-                       dateDelivery = it.result.dateDelivery,
-                       importTotal = calculateTotalImport(it.result.listOrderProducts).toString(),
-                       unitTotal =  calculateTotalUnit(it.result.listOrderProducts) .toString(),
-                       listOrderProducts = it.result.listOrderProducts
-
-                   )
-
-
-                    NetworkResponseState.Success(mapOrderDetail)
+                    NetworkResponseState.Success(it.result.toOrderDetail())
                 }
             }
 
@@ -65,23 +57,3 @@ class OrderRepositoryImpl(
     }
 }
 
-fun calculateTotalUnit(listOrderProducts: List<OrderProducts>): Float {
-
-    var totalUsd = 0.0f
-
-    listOrderProducts.forEach { product ->
-        totalUsd += product.unit.toInt()
-    }
-    return totalUsd
-}
-
-fun calculateTotalImport(listOrderProducts: List<OrderProducts>): Float {
-
-    var totalUsd = 0.0f
-
-    listOrderProducts.forEach { product ->
-        val priceUsd = product.priceUsd.toFloatOrNull() ?: 0.0f
-        totalUsd += priceUsd * product.unit.toInt()
-    }
-    return totalUsd
-}
